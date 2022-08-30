7c47530b75d86f0b0e76ee844c270e41ee360e7e
Lorenzo Franco <lfranco@student.neumont.edu>
2022-08-29 22:29:40 -0600

    Add project files.

---
+++ Engine/Core/File.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+	void setFilePath(const std::string& pathname);
+	std::string getFilePath();
+
+	bool fileExists(const std::string& pathname);
+	bool getFileSize(const std::string& pathname, size_t& size);
+	bool readFile(const std::string& pathname, std::string& buffer);
+}
